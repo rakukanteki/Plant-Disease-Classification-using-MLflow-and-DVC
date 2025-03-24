23f6a8bac4df00dea675c70726b3e884d5dfe5d8
Radwan <radwankhondokar20@gmail.com>
2025-03-24 16:58:17 +0600

    model dir added

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -10,7 +10,7 @@ class PredictionPipeline:
 
     def predict(self):
         # Load the model
-        model = load_model(os.path.join("artifacts", "training", "model.h5"))
+        model = load_model(os.path.join("model", "model.h5"))
 
         # Load and preprocess the image
         test_image = image.load_img(self.filename, target_size=(224, 224))
